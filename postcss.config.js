4b134281ce5fdd32e1e64cd9aa3467fafeee2bdc
one-media-asia <onemediaasia@duck.com>
2026-04-23 23:53:39 +0800

    s

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,7 @@
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
+// postcss.config.js
 export default {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-  ],
-};
-<<<<<<< Updated upstream
-//
-=======
-// 
->>>>>>> Stashed changes
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
